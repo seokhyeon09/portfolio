eace68fc33e4bb0dfd952eadec922f620f30a1e9
seokhyeon09 <ring4392@gmail.com>
2026-05-11 18:46:03 +0900

    into

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Home from "./pages/home/Home"
-import About from './pages/about/About'
-import ProjectsPages from "./pages/projects/ProjectsPages"
-
+import { BrowserRouter,Navigate,Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from './pages/About'
+import Contact from './pages/Contact'
+import ProjectsPages from "./pages/ProjectsPages"
+import Skills from "./pages/Skills"
 import PlaceholderPage from "./pages/PlaceholderPage"
-import Blog from "./pages/blog/Blog"
+import Blogs from './pages/Blogs'
 import  ThemeProvider from './context/ThemeProvider'
-import SkillsPages from "./pages/skills/SkillsPages"
-import ContactPage from "./pages/contact/ContactPage"
 
 import Layout from './components/layout/Layout'
 
-function NotFound() {
-  return (
+function NotFound(){
+  return(
     <PlaceholderPage
       title="Page not found"
       desc="존재하지 않는 페이지 입니다."
@@ -27,25 +26,24 @@ function App() {
 
   return (
     <BrowserRouter>
-      <ThemeProvider>
+    <ThemeProvider>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="projects" element={<ProjectsPages/>}/>
+          <Route path="skills" element={<Skills/>}/>
+          <Route path="blogs" element={<Blogs/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="privacy" element={<PlaceholderPage title="Privacy"/>}/>
+          <Route path="terms" element={<PlaceholderPage title="Terms"/>}/>
+          <Route path="blogs" element={<Blogs/>}/>
+          <Route path="404" element={<NotFound/>}/>
+          <Route path="*" element={<Navigate to="/404" replace/>}/>
 
-        <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="projects" element={<ProjectsPages />} />
-            <Route path="skills" element={<SkillsPages />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="contact" element={<ContactPage />} />
-            <Route path="privacy" element={<PlaceholderPage title="Privacy" />} />
-            <Route path="terms" element={<PlaceholderPage title="Terms" />} />
-            <Route path="404" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-
-          </Route>
-        </Routes>
-      </ThemeProvider>
-
+        </Route>
+      </Routes>
+  </ThemeProvider>
     </BrowserRouter>
   )
 }
